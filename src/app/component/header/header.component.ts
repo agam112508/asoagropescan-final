7395bef468f2f64002a459bcb516598abe0fed1f
Esteban Agamez <eagamezmontero@gmail.com>
2024-06-22 17:18:23 -0500

    gregando icon de fabiicon

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -9,7 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
   imagePath: string = 'assets/images/logo_png.png';
   isOpen = false;
 
